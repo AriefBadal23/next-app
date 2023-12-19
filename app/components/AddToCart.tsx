dfff3ec32c639b99260e6f2b1ad231d87445919b
Arief Badal <1042170@hr.nl>
2023-12-19 16:19:14 +0100

    Add component with API request

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log("Click")}>Add to Cart</button>
     </div>
   );
 };
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+// server component
 
-export const UsersPage = () => {
-  return (
-    <div>UsersPage</div>
-  )
+interface User {
+  id: number;
+  name: string;
 }
 
-export default  UsersPage;
+export const NewUserPage = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users", {cache:'no-store'});
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toTimeString()}</p>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+export default NewUserPage;
